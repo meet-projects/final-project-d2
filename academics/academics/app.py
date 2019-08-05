81c472b56aee0539f93f86b5e1c55e1b3c1912b1
myname21-meet <myname21@meet.mit.edu>
2019-08-05 10:19:54 -0400

    added

--- academics/academics/app.py
+++ academics/academics/app.py
@@ -1,13 +1,17 @@
+from database import *
 from flask import Flask, render_template,url_for
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return render_template("index.html")
+	ls = session.query(Product).all()
+	return render_template("index.html",ls=ls)
 
 @app.route('/store')
 def store():
-	return render_template("courses.html")
+	ls = session.query(Product).all()
+	return render_template("courses.html",ls =ls)
 
 @app.route('/contact')
 def contact():
@@ -20,6 +24,11 @@ def about():
 @app.route('/stories')
 def stories():
 	return render_template("read.html")
+
+@app.route('/product/<int:product_id>')
+def display_product(product_id):
+	product = query_by_id(product_id)
+	return render_template("course-single.html", product = product)
 
 
 if __name__ == '__main__':
--- academics/academics/database.py
+++ academics/academics/database.py
@@ -1,9 +1,9 @@
-from model import Base, Rating
+from model import Base, Product
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///students.db?check_same_thread=False')
+engine = create_engine('sqlite:///products.db?check_same_thread=False')
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
@@ -15,3 +15,8 @@ def add_product(picture_path,name,price):
 		price = price)
 	session.add(product_object)
 	session.commit()
+
+def query_by_id(product_id):
+    product = session.query(Product).filter_by(
+        product_id=product_id).first()
+    return product
